e9d14ddc66e907f11bd039ab134e2f78232dd93c
Alsu32 <sai-alsu@mail.ru>
2022-10-01 22:35:09 +0300

    JS Test11-20

---
+++ JS_Test 11-20.js
@@ -0,0 +1,35 @@
+Test 11
+const sum = 10 + 10;
+
+Test 12
+const difference = 45 - 33;
+
+Test 13
+const product = 8 * 10;
+
+Test 14
+const quotient = 66 / 33;
+
+Test 15
+let myVar = 87;
+// Only change code below this line
+myVar++;
+
+Test 16
+let myVar = 11;
+// Only change code below this line
+myVar--;
+
+Test 17
+const ourDecimal = 5.7;
+// Only change code below this line
+let myDecimal = 5.7;
+
+Test 18
+const product = 2.0 * 2.5;
+
+Test 19
+const quotient = 4.4 / 2.0; // Change this line
+
+Test 20
+const remainder = 11 % 3;
