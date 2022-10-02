603583faaa2d40fa7ad79eb0ca9a7beecb581389
Alsu32 <sai-alsu@mail.ru>
2022-10-02 16:26:37 +0300

    JS_Test(36-40)

---
+++ JS_Test(36-40).js
@@ -0,0 +1,29 @@
+Test36
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+Test37
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+Test38
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+// Only change code below this line
+const wordBlanks = "MyNoun: " + myNoun + ", myAdjective: " + myAdjective + ", myVerb: " + myVerb + ", myAdverb: " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+Test39
+// Only change code below this line
+const myArray = ["look", 22];
+
+Test40
+// Only change code below this line
+const myArray = [["cat", 4], ["dog"], 5];
