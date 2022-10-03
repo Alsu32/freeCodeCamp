f462d75eea593c08785e723c07f166a0b36ad7f2
Alsu32 <sai-alsu@mail.ru>
2022-10-03 10:58:04 +0300

    JS_Test(46-50)

---
+++ JS_Test(46-50).js
@@ -0,0 +1,33 @@
+Test46
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+let removedFromMyArray = myArray.shift();
+
+Test47
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+Test48
+const myList = [
+  ["Chocolate Bar", 15],
+  ["Milk", 2],
+  ["Bread", 1],
+  ["Meat", 1],
+  ["Apple", 5]
+];
+
+Test49
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
+
+Test50
+function functionWithArgs(num1, num2) {
+  console.log(num1 + num2);
+}
+functionWithArgs(1, 2);
